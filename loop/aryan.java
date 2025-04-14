f10b0c8188928c30249f4de6205b0e274dd6f9c6
aryan5333 <sinhaaryan5333@gmail.com>
2025-04-14 22:45:43 +0530

    add aryan.java for basic positive/negative number check

---
+++ loop/aryan.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+public class aryan {
+    public static void main(String[] args) 
+        {
+             int n=-5;
+             if(n>=0)
+             {
+                System.out.println("positive");
+
+             }
+             else
+             {
+                System.out.println("negative");
+             }
+
+            
+        }
+    
+
+    
+}
