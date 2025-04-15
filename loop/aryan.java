134affaf03379aa4c4e3c95dc6e43eefd4c74f99
aryan5333 <sinhaaryan5333@gmail.com>
2025-04-15 23:37:52 +0530

    refactor aryan.java to implement maximum number finder logic

--- loop/aryan.java
+++ loop/aryan.java
@@ -1,21 +1,30 @@
 import java.lang.*;
-public class aryan {
-    public static void main(String[] args) 
-        {
-             int n=-5;
-             if(n>=0)
-             {
-                System.out.println("positive");
 
-             }
-             else
-             {
-                System.out.println("negative");
-             }
+class aryan{
+   public static void main(String[]args)
+   {
+      int a=5,b=6,c=4;
+      if(a>b&&a>c)
+      {
+         System.out.println(a);
+      }
+      else
+      if(b>c)
+      {
+         System.out.println(b);
+      }
+      else{
+         System.out.println(c);
+      }
+      
+         
+      }
+   }
+      
 
-            
-        }
-    
+      
+      
+      
 
-    
-}
+   
+
