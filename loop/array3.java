fc62f29bc78dfaf2ac33fd0f8f6c098f21b392b4
aryan5333 <sinhaaryan5333@gmail.com>
2025-04-24 20:05:53 +0530

    refactor array3.java to rotate array elements and remove unused code

--- loop/array3.java
+++ loop/array3.java
@@ -1,32 +1,24 @@
 import java.lang.*;
-import java.util.Scanner;
-
+import java.util.Scanner; 
 
 class array3
 {
     public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int A[]={1,2,5,8,9,12,18};
+        for(int x:A)
+        System.out.println(x+"");
+        System.out.println("");
+        int temp=A[0];
+        for(int i=1;i<A.length;i++)
         {
-            Scanner sc=new Scanner(System.in);
-
-            int A[]={1,2,4,8,6,10,17,14};
-            int max=A[0];
-
-            for(int i=0;i<A.length;i++)
-            {
-                if(A[i]>max)
-                {
-                    max=A[i];
-                }
-
-            }
-            System.out.println(max);
-
-
-            
-
-            
-        
-            
-
+            A[i-1]=A[i];
         }
+        A[A.length-1]=temp;
+        for(int x:A)
+        System.out.println(x+",");
+        System.out.println("");
+    }
+        
     }
