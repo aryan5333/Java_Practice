4be1df1239014d5e8d1ea2190472765cad3c7ca2
aryan5333 <sinhaaryan5333@gmail.com>
2025-04-28 22:23:50 +0530

    add method3.java and method4.java to implement area calculations and name validation

---
+++ loop/method3.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+
+class method3
+{
+    static double area( double length ,double  breath)
+    {
+        return length*breath;
+
+    }
+    static double area(double radius)
+    {
+        return 3.14*radius*radius;
+    }
+    public static void main(String[] args) {
+        System.out.println("Area");
+    
+    }
+        
+    
+
+}
