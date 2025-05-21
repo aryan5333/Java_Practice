9cf7f05de39835f27770d2302c602f2c32c661e6
aryan5333 <sinhaaryan5333@gmail.com>
2025-05-21 20:18:13 +0530

    Add Resource and StackDemo classes with exception handling

---
+++ loop/Resource.java
@@ -0,0 +1,25 @@
+class Resource
+{
+    static void display()
+    {
+        int i = 1;
+        while (true)
+        {
+            System.out.println(i + " Hello");
+            i++;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        display();
+        int i=1;
+        while (true)
+        {
+            System.out.println(i+"world");
+            i++;
+        } 
+            
+        }
+    }
+
