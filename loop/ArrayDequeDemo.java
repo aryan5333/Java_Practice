5b8b15d285cf104db0d0d9a7900c9860fc3a47cd
aryan5333 <sinhaaryan5333@gmail.com>
2025-06-04 22:37:17 +0530

    Add ArrayDequeDemo, Arraylist, and MyLinkedListDemo classes with examples of Java collection operations

---
+++ loop/ArrayDequeDemo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+
+public class ArrayDequeDemo
+{
+    public static void main(String[]args)
+    {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+
+        ad.offerLast(30);
+        ad.offerLast(40);
+        ad.offerLast(50);
+        ad.offerLast(60);
+
+        ad.pollFirst();
+         ad.forEach((x) -> System.out.println(x));
+
+        /*ad.offerFirst(4);
+        ad.offerFirst(6);
+        ad.offerFirst(5);
+        ad.offerFirst(4);
+
+        ad.forEach((x) -> System.out.println(x)); */
+    }
+}
---
+++ loop/MyLinkedListDemo.java
@@ -0,0 +1,46 @@
+import java.lang.*;
+import java.security.AllPermission;
+import java.util.*;
+
+public class MyLinkedListDemo
+{
+    public static void main(String[] args) {
+        LinkedList<Integer> al1=new LinkedList<>();
+
+        LinkedList<Integer> al2=new LinkedList<>(List.of(20,30,40,50,60));
+
+        al1.add(10);
+        al1.add(0,5 );
+        al1.addAll(1,al2);
+        al1.add(5,40);
+
+
+        al1.set(6, 90);
+
+        ListIterator<Integer> it= al1.listIterator() ;
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+
+        al1.forEach(n->show(n));
+        al1.addFirst(3);
+        al1.addLast(200);
+        
+
+        
+
+        /*for(int i=0;i<al1.size();i++)
+        {
+            System.out.println(al1.get(i));
+        } */
+
+    }
+    public static void show(int i)
+    {
+        //if(i>30)
+        {
+           System.out.println(i);
+        }
+    }
+}
