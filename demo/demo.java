d0f69dae3c83a5dd1a826b8e1106f5cef0725d7b
aryan5333 <sinhaaryan5333@gmail.com>
2025-09-17 01:08:44 +0530

    Initial commit of demo.java with array sorting implementation

---
+++ demo/demo.java
@@ -0,0 +1,15 @@
+package demo;
+
+import java.util.*;
+
+ public class demo
+ {
+    public static void main(String[]args)
+    {
+        int[]arr=new int[]{10,5,4,12,8,3};
+        Arrays.sort(arr,3,5);
+        System.out.println(Arrays.toString(arr));
+
+    }
+ }
+
