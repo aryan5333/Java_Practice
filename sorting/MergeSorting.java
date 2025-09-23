1a5844ed5ba985db93a3fb6ec36be07dd825680e
aryan5333 <sinhaaryan5333@gmail.com>
2025-09-23 19:27:31 +0530

    Add MergeSorting and QuickSort classes with basic sorting implementations

---
+++ sorting/MergeSorting.java
@@ -0,0 +1,54 @@
+public class MergeSorting {
+    public static void conquer( int arr[],int si,int mid,int ei){
+        int merged[]=new int[ei-si+1];
+
+        int idx1=si;
+        int idx2=mid+1;
+        int x=0;
+
+        while(idx1 <= mid && idx2<=ei){
+            if(idx1<=idx2){
+                merged[x++]=arr[idx1++];
+               
+            }
+            else{
+                merged[x++]=arr[idx1++];
+                
+            }
+        }
+        while(idx1<=mid){
+            merged[x++]=arr[idx1++];
+            
+        }
+        while(idx2<=ei){
+            merged[x++]=arr[idx2++];
+        }
+        for(int i=0,j=si;i<merged.length;i++,j++){
+            arr[j]=merged[i];
+
+        }
+    }
+    public static void divide(int arr[],int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid=si+(si-ei)/2;
+        divide(arr,si,mid);
+        divide(arr,mid+1,ei);
+        conquer(arr,si,mid,ei);
+
+    }
+    public static void main(String[]args){
+        int arr[]={2,10,4,56,28,45,36};
+        int n=arr.length;
+
+        divide(arr,0,n-1);
+        for(int i=1;i<n;i++){
+            System.out.println(arr[i]+"");
+        }
+        System.out.println();
+       
+        }
+    }
+    
+
