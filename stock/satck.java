26527d29e76a60197cb4e1c22c282c870d05110d
aryan5333 <sinhaaryan5333@gmail.com>
2025-09-26 20:52:18 +0530

    Add Stack class implementation with basic push, pop, and peek operations

---
+++ stock/satck.java
@@ -0,0 +1,21 @@
+package stock;
+import java.lang.*;
+import java.util.*;
+public class satck {
+    public static void main(String[] args) {
+        Stack<Integer>stack=new Stack<>();
+
+        stack.push(10);
+        stack.push(50);
+        stack.push(20);
+        stack.push(40);
+        stack.push(30);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+
+        
+    }
+
+    
+}
