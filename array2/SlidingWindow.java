bb9f3ad8255dccc1382be49f5b5cdfd85941f91e
aryan5333 <sinhaaryan5333@gmail.com>
2025-10-03 22:08:15 +0530

    Update array values in SlidingWindow for correct sum calculation

--- array2/SlidingWindow.java
+++ array2/SlidingWindow.java
@@ -2,7 +2,7 @@ package array2;
 
 public class SlidingWindow {
     public static void main(String[] args) {
-        int[]arr={10,2,7,8,11,13};
+        int[]arr={10,20,50,60,70,80};
         int sum=0,n=arr.length;
         int k=3;
         for(int i=0;i<k;i++){
