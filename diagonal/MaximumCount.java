24bd8009a4308896ecb31e54bfaad8248998a474
aryan5333 <sinhaaryan5333@gmail.com>
2025-10-09 21:11:57 +0530

    maximumcount

---
+++ diagonal/MaximumCount.java
@@ -0,0 +1,27 @@
+package diagonal;
+
+public class MaximumCount {
+    int n = nums.length;
+        int i = lowerBound(nums, 1);  // first index with value ≥ 1
+        int j = lowerBound(nums, 0);  // first index with value ≥ 0
+
+        int posCount = n - i;
+        int negCount = j;
+
+        return Math.max(posCount, negCount);
+    }
+
+    
+    private int lowerBound(int[] nums, int target) {
+        int left = 0, right = nums.length; 
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    
+}
