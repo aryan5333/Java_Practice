e8d4f052a2f9c6d9cd1ce64029da7b1db3216ace
aryan5333 <sinhaaryan5333@gmail.com>
2025-11-11 22:27:49 +0530

    Refactor BinaryTree class to use dynamic array initialization and improve pre-order traversal method

--- binarytree/BinaryTree.java
+++ binarytree/BinaryTree.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 
 public class BinaryTree {
-    private int[] arr = new int[]{10,20,30,40,50,60};
+    private int[] tree;
 
-    public BinaryTree(int n){
-        this.arr = new int[n];
+    public BinaryTree(int [] intValues){
+        this.tree = intValues;
         // print array contents after initialization
-        System.out.println(Arrays.toString(this.arr));
+        System.out.println(Arrays.toString(this.tree));
     }
     private int left(int i){
         return 2*i+1;
@@ -19,6 +19,23 @@ public class BinaryTree {
     private int parent(int i){
         return (i-1)/2;
     }
+    private void preOrderTraversal(int nodeNo){
+        if(nodeNo <this.tree.length){
+        System.out.println(this.tree[nodeNo]);
+        preOrderTraversal(this.left(nodeNo));
+        preOrderTraversal(this.right(nodeNo));
+        }
+    }
+    public void preOrderTraversal(){
+        preOrderTraversal(0);
 
+    }
+
+}
+class Application{
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree(new int[]{10,20,30,40,50,60,70,80});
+        tree.preOrderTraversal();
+    }
 }
 
